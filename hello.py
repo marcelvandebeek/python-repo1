e06dbe0534793afe5caf92e3da0e1adfd2f08662
marcel <marcel.vandebeek@zuyd.nl>
2025-03-10 20:06:53 +0000

    REDO2

--- hello.py
+++ hello.py
@@ -1,4 +1,4 @@
- pylint: disable=missing-module-docstring
+# pylint: disable=missing-module-docstring
 # pylint: disable=missing-class-docstring
 # pylint: disable=missing-function-docstring
 # pylint: disable=invalid-name
