095ddb945f430471e33077fa0335461e669d394d
marcelvandebeek <87375287+marcelvandebeek@users.noreply.github.com>
2025-03-10 15:32:32 +0100

    Update lint.py

--- lint.py
+++ lint.py
@@ -1,9 +1,17 @@
+# pylint: disable=missing-module-docstring
+# pylint: disable=missing-class-docstring
+# pylint: disable=missing-function-docstring
+# pylint: disable=invalid-name
+# pylint: disable=trailing-newlines
+# pylint: disable=trailing-whitespace
+# pylint: disable=missing-final-newline
+
 import sys
 from pylint import lint
 THESHOLD = 9
 run = lint.Run(["hello.py"], exit=False)
 score = run.linter.stats.global_note
 if score < THESHOLD:
-  print("Linter failed: Score < threshold value")
-  sys.exit(1)
+    print("Linter failed: Score < threshold value")
+    sys.exit(1)
 sys.exit(0)
