4d6e8690e631aa5a95aaccfd1198381b7a4fdf2f
marcelvandebeek <87375287+marcelvandebeek@users.noreply.github.com>
2025-03-10 15:30:49 +0100

    Update hello.py

--- hello.py
+++ hello.py
@@ -1,2 +1,8 @@
+# pylint: disable=missing-module-docstring
+# pylint: disable=missing-class-docstring
+# pylint: disable=missing-function-docstring
+# pylint: disable=invalid-name
 # pylint: disable=trailing-newlines
+# pylint: disable=trailing-whitespace
+# pylint: disable=missing-final-newline
 print("Hello world")
